aead3d236468977209bbfbbcb58c47b28e53832d
pratyaysaha <pratyaysaha@outlook.com>
2020-11-05 01:10:18 +0530

    searchbyName is added

--- test-1.cpp
+++ test-1.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main()
 {
     char a[100];
-    strcpy(a,"pop123");
+    strcpy(a,"pratyay");
 
     admin_database db;
-    vector<admin> info=db.searchbyAdminID(a);
+    vector<admin> info=db.searchbyName(a);
     for(int i=0;i<info.size();i++)
     {
         info[i].display();
